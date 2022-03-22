733439417a566721f2f984911ac39f16639c0c96
Thimble <anthonyeboothroyd@gmail.com>
2022-03-22 11:06:58 +0000

    Revert "fixed chunk"
    This reverts commit 8b19c463ad06b13b2f56d293e135bcaf2f64be84.

--- Reldawin Unity/Assets/Resources/Shaders/TerrainShader.shader
+++ Reldawin Unity/Assets/Resources/Shaders/TerrainShader.shader
@@ -7,102 +7,96 @@
 		[MaterialToggle] Premultiplied_Alpha("Premultiplied Alpha", Int) = 1
 		[MaterialToggle] PixelSnap("Pixel snap", Float) = 0
 	}
-
-		SubShader
+	SubShader
+	{
+		Tags
 		{
-			Tags
-			{
-				//"Queue" = "Transparent" // Line removed to fix rendering outlines
-				"IgnoreProjector" = "True"
-				"RenderType" = "Transparent"
-				"PreviewType" = "Plane"
-				"CanUseSpriteAtlas" = "True"
-			}
-
-			Cull Off
-			Lighting Off
-			ZWrite Off
-			BlendOp Add
-			Blend One OneMinusSrcAlpha
-
-			Pass
-			{
-				CGPROGRAM
-
-				#pragma vertex VertexFunc
-				#pragma fragment FragmentFunc
-				#pragma multi_compile _ PIXELSNAP_ON
-				#pragma multi_compile _ PREMULTIPLIED_ALPHA_ON
-				#include "UnityCG.cginc"
-
-				struct appdata_t
-				{
-					float4 vertex   : POSITION;
-					float4 color    : COLOR;
-					float2 uv_bottomLayer  : TEXCOORD0;
-					float2 uv_topLayer   : TEXCOORD1;
-				};
-
-				struct v2f
-				{
-					float4 vertex   : SV_POSITION;
-					fixed4 color : COLOR;
-					float2 uv_bottomLayer  : TEXCOORD0;
-					float2 uv_topLayer   : TEXCOORD1;
-				};
-
-				fixed4 _Color;
-
-				v2f VertexFunc(appdata_t IN)
-				{
-					v2f OUT;
-					OUT.vertex = UnityObjectToClipPos(IN.vertex);
-					OUT.uv_bottomLayer = IN.uv_bottomLayer;
-					OUT.uv_topLayer = IN.uv_topLayer;
-					OUT.color = IN.color * _Color;
-					#ifdef PIXELSNAP_ON
-					OUT.vertex = UnityPixelSnap(OUT.vertex);
-					#endif
-
-					return OUT;
-				}
-
-				sampler2D _MainTex;
-				sampler2D _AlphaTex;
-
-				fixed4 SampleSpriteTexture(float2 uv)
-				{
-					fixed4 color = tex2D(_MainTex, uv);
-
-					return color;
-				}
-
-				fixed4 FragmentFunc(v2f IN) : SV_Target
-				{
-					fixed4 background = SampleSpriteTexture(IN.uv_bottomLayer) * _Color;
-					fixed4 foreground = SampleSpriteTexture(IN.uv_topLayer) * _Color;
-
-					fixed topAlpha = 1 - foreground.a;
-					fixed botAlpha = 1 - background.a;
-
-					if (foreground.a > 0)
-					{
-						fixed4 finalColor = { 0, 0, 0, 0 };
-
-						#ifdef PREMULTIPLIED_ALPHA_ON
-						finalColor.rgb = (foreground.rgb) + (background.rgb * (1 - foreground.a));
-						#else
-						finalColor.rgb = (foreground.rgb * foreground.a) + (background.rgb * (1 - foreground.a));
-						#endif
-						//return alpha
-						return finalColor;
-					}
-
-					//return opaque
-					return background;
-				}
-
-				ENDCG
-			}
+			"IgnoreProjector" = "True"
+			"RenderType" = "Transparent"
+			"PreviewType" = "Plane"
+			"CanUseSpriteAtlas" = "True"
 		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		BlendOp Add
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex VertexFunc
+			#pragma fragment FragmentFunc
+			#pragma multi_compile _ PIXELSNAP_ON
+			#pragma multi_compile _ PREMULTIPLIED_ALPHA_ON
+			#include "UnityCG.cginc"
+
+			struct appdata_t
+			{
+				float4 vertex   : POSITION;
+				float4 color    : COLOR;
+				float2 uv_bottomLayer  : TEXCOORD0;
+				float2 uv_topLayer   : TEXCOORD1;
+			};
+
+			struct v2f
+			{
+				float4 vertex   : SV_POSITION;
+				fixed4 color : COLOR;
+				float2 uv_bottomLayer  : TEXCOORD0;
+				float2 uv_topLayer   : TEXCOORD1;
+			};
+
+			fixed4 _Color;
+
+			v2f VertexFunc(appdata_t IN)
+			{
+				v2f OUT;
+				OUT.vertex = UnityObjectToClipPos(IN.vertex);
+				OUT.uv_bottomLayer = IN.uv_bottomLayer;
+				OUT.uv_topLayer = IN.uv_topLayer;
+				OUT.color = IN.color * _Color;
+				#ifdef PIXELSNAP_ON
+				OUT.vertex = UnityPixelSnap(OUT.vertex);
+				#endif
+
+				return OUT;
+			}
+
+			sampler2D _MainTex;
+			sampler2D _AlphaTex;
+
+			fixed4 SampleSpriteTexture(float2 uv)
+			{
+				fixed4 color = tex2D(_MainTex, uv);
+
+				return color;
+			}
+			fixed4 FragmentFunc(v2f IN) : SV_Target
+			{
+				fixed4 background = SampleSpriteTexture(IN.uv_bottomLayer) * _Color;
+				fixed4 foreground = SampleSpriteTexture(IN.uv_topLayer) * _Color;
+
+				fixed topAlpha = 1 - foreground.a;
+				fixed botAlpha = 1 - background.a;
+
+				if (foreground.a > 0)
+				{
+					fixed4 finalColor = { 0, 0, 0, 0 };
+
+					#ifdef PREMULTIPLIED_ALPHA_ON
+					finalColor.rgb = (foreground.rgb) + (background.rgb * (1 - foreground.a));
+					#else
+					finalColor.rgb = (foreground.rgb * foreground.a) + (background.rgb * (1 - foreground.a));
+					#endif
+					//return alpha
+					return finalColor;
+				}
+				return background;
+			}
+			ENDCG
+		}
+	}
 }
--- Reldawin Unity/Assets/Scripts/Terrain/Chunk.cs
+++ Reldawin Unity/Assets/Scripts/Terrain/Chunk.cs
@@ -1,174 +1,108 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-namespace LowCloud.Reldawin
+﻿Shader "LowCloud/TerrainShader"
 {
-    public class Chunk : MonoBehaviour
-    {
-        public DoodadLoader doodadLoader;
-        public List<Doodad> activeDoodads = new List<Doodad>(); //public for testing purposes
-        public MeshFilter meshFilter;
-        public Vector2Int ChunkIndex;
-        public Tile[,] Tiles { get; set; }
-        public Node[,] Nodes { get; set; }
+	Properties
+	{
+		_MainTex( "Sprite Texture", 2D ) = "white" { }
+		_Color( "Tint", Color ) = (1, 1, 1, 1)
+		[MaterialToggle] Premultiplied_Alpha( "Premultiplied Alpha", Int ) = 1
+		[MaterialToggle] PixelSnap( "Pixel snap", Float ) = 0
+	}
 
-        public void CreateTiles( Vector2Int chunkIndex, string data )
-        {
-            transform.position = MyMath.CellToIsometric( chunkIndex ) * Chunk.Size;
-            this.ChunkIndex = chunkIndex;
-            Tiles = new Tile[Chunk.Size + 2, Chunk.Size + 2];
-            Nodes = new Node[Chunk.Size, Chunk.Size];
+	SubShader
+		{
+		Tags
+			{
+			//"Queue" = "Transparent" // Line removed to fix rendering outlines
+			"IgnoreProjector" = "True"
+				"RenderType" = "Transparent"
+				"PreviewType" = "Plane"
+				"CanUseSpriteAtlas" = "True"
+			}
 
-            int index = 0;
+		Cull Off
+			Lighting Off
+			ZWrite Off
+			BlendOp Add
+			Blend One OneMinusSrcAlpha
 
-            for ( ushort _y = 0; _y < Chunk.Size + 2; _y++ )
-                for ( ushort _x = 0; _x < Chunk.Size + 2; _x++ )
-                {
-                    Vector2Int cellPosition = new Vector2Int( _x, _y );
+			Pass
+			{
+			CGPROGRAM
 
-                    Tiles[_x, _y] = Tile.GetTileByIndex( data[index++] );
-                    Tiles[_x, _y].CellPositionInWorld = ( chunkIndex * Chunk.Size + cellPosition );
-                    Tiles[_x, _y].CellPositionInChunk = cellPosition;
-                }
+				#pragma vertex VertexFunc
+#pragma fragment FragmentFunc
+#pragma multi_compile _ PIXELSNAP_ON
+#pragma multi_compile _ PREMULTIPLIED_ALPHA_ON
+# include "UnityCG.cginc"
 
-            for ( ushort _y = 0; _y < Chunk.Size; _y++ )
-                for ( ushort _x = 0; _x < Chunk.Size; _x++ )
-                {
-                    Nodes[_x, _y] = new Node()
-                    {
-                        type = -1,
-                        CellPositionInWorld = Tiles[_x, _y].CellPositionInWorld
-                    };
-                }
+struct appdata_t
+{
+	float4 vertex   : POSITION;
+					float4 color    : COLOR;
+					float2 uv_bottomLayer  : TEXCOORD0;
+					float2 uv_topLayer   : TEXCOORD1;
+				};
 
-            SetUVChannel( 0 );  //regular tiles
-            SetUVChannel( 1 ); //corners that overlap
+struct v2f
+{
+	float4 vertex   : SV_POSITION;
+					fixed4 color : COLOR;
+					float2 uv_bottomLayer  : TEXCOORD0;
+					float2 uv_topLayer   : TEXCOORD1;
+				};
 
-            gameObject.name = "Enabled Chunk";
-            gameObject.SetActive( true );
+fixed4 _Color;
 
-            ClientTCP.SendChunkDoodadQuery( chunkIndex );
-        }
+v2f VertexFunc( appdata_t IN )
+{
+	v2f OUT;
+	OUT.vertex = UnityObjectToClipPos( IN.vertex );
+	OUT.uv_bottomLayer = IN.uv_bottomLayer;
+	OUT.uv_topLayer = IN.uv_topLayer;
+	OUT.color = IN.color * _Color;
+# ifdef PIXELSNAP_ON
+	OUT.vertex = UnityPixelSnap( OUT.vertex );
+#endif
 
-        public void SetUVChannel( int channel )
-        {
-            List<Vector2> uvs = new List<Vector2>();
-            Mesh.GetUVs( 0, uvs ); // Get layer zero as its uvs are preset
+	return OUT;
+}
 
-            for ( int _x = 1; _x < Chunk.Size + 1; _x++ )
-            for ( int _y = 1; _y < Chunk.Size + 1; _y++ )
-            {
-                Tile[] neighbours = GetNeighbours( _x, _y );
+sampler2D _MainTex;
+sampler2D _AlphaTex;
 
-                Vector2[] UVs = SpriteLoader.GetTileUVs( Tiles[_x, _y].TileType, neighbours );                            
+fixed4 SampleSpriteTexture( float2 uv )
+{
+	fixed4 color = tex2D( _MainTex, uv );
 
-                //_x and _y are decreased by 1 because the nested for loops above do shit. 
-                int r = ( ( _x - 1 ) * Chunk.Size + ( _y - 1 ) ) * 4;
+	return color;
+}
 
-                for ( int index = 0; index < 4; index++ )
-                {
-                    uvs[r + index] = UVs[index];
-                }
-            }
+fixed4 FragmentFunc( v2f IN ) : SV_Target
+{
+	fixed4 background = SampleSpriteTexture( IN.uv_bottomLayer ) * _Color;
+	fixed4 foreground = SampleSpriteTexture( IN.uv_topLayer ) * _Color;
 
-            Mesh.SetUVs( channel, uvs );
-        }
+	fixed topAlpha = 1 - foreground.a;
+	fixed botAlpha = 1 - background.a;
 
-        private void OnMouseDown()
-        {
-            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+	if ( foreground.a > 0 )
+	{
+		fixed4 finalColor = { 0, 0, 0, 0 };
 
-            if ( Physics.Raycast( ray, out RaycastHit hit ) && hit.transform.CompareTag( "Terrain" ) && hit.transform == transform )
-            {
-                Vector2Int p = MyMath.IsometricToCell( hit.point );
+# ifdef PREMULTIPLIED_ALPHA_ON
+		finalColor.rgb = ( foreground.rgb ) + ( background.rgb * ( 1 - foreground.a ) );
+#else
+		finalColor.rgb = ( foreground.rgb * foreground.a ) + ( background.rgb * ( 1 - foreground.a ) );
+#endif
+		//return alpha
+		return finalColor;
+	}
 
-                if ( Input.GetMouseButtonDown( 0 ) )
-                {
-                    if ( EventSystem.current.IsPointerOverGameObject() )
-                    {
+	//return opaque
+	return background;
+}
 
-                    }
-                    else
-                    {
-                        OnClicked?.Invoke( p, hit.point );
-                    }
-                }
-            }
-        }
-
-        public void SetDoodads( List<Doodad.Data> doodads, List<Doodad> inactiveDoodads )
-        {
-            foreach ( Doodad.Data data in doodads )
-            {
-                inactiveDoodads[0].Setup( data );
-                inactiveDoodads[0].transform.SetParent( transform ); // something like this
-                activeDoodads.Add( inactiveDoodads[0] );
-                int x = data.tilePositionInChunk.x;
-                int y = data.tilePositionInChunk.y;
-                inactiveDoodads.RemoveAt( 0 );
-
-                Nodes[x, y].type = data.type;
-            }
-        }
-
-        public Tile[] GetNeighbours( int x, int y )
-        {
-            try
-            {
-
-                Tile[] neighbors = new Tile[8]
-                {
-                 Tiles[x + 0, y + 1], //forward
-                 Tiles[x + 1, y + 0], //right
-                 Tiles[x + 0, y - 1], //down
-                 Tiles[x - 1, y + 0], //left
-                 Tiles[x + 1, y + 1], //forard-right
-                 Tiles[x + 1, y - 1], //back-right
-                 Tiles[x - 1, y - 1], //back-left
-                 Tiles[x - 1, y + 1]  //forward-left
-                };
-                return neighbors;
-
-            }
-            catch ( System.Exception )
-            {
-                Debug.LogError( string.Format( "{0} , {1}", x, y ) );
-            }
-
-            return null;
-        }
-
-        public void Disable()
-        {
-            gameObject.name = "Disabled Chunk";
-            gameObject.SetActive( false );
-
-            foreach ( Doodad doodad in activeDoodads )
-            {
-                doodad.gameObject.SetActive( false );
-            }
-
-            OnChunkDestroyed?.Invoke( ChunkIndex, activeDoodads );
-            activeDoodads.Clear();
-        }
-
-        public const ushort Size = 30;
-
-        public static event ClickAction OnClicked;
-
-        public static event ChunkDestroyImminent OnChunkDestroyed;
-
-        public delegate void ClickAction( Vector2Int cellClicked, Vector2 pointClicked );
-
-        public delegate void ChunkDestroyImminent( Vector2Int chunkIndex, List<Doodad> doodads );
-
-        private Mesh Mesh
-        {
-            get
-            {
-                return meshFilter.mesh;
-            }
-        }
-    }
+ENDCG
+			}
+		}
 }
