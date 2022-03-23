f8d84f4483c2d1cb878d7c913248778d41b9fa0b
Thimble <anthonyeboothroyd@gmail.com>
2022-03-23 03:25:38 +0000

    Update XMLLoader.cs

--- Reldawin Unity/Assets/Scripts/XMLLoader/XMLLoader.cs
+++ Reldawin Unity/Assets/Scripts/XMLLoader/XMLLoader.cs
@@ -8,46 +8,9 @@ namespace LowCloud.Reldawin
 {
     public class XMLLoader
     {
-        public static Dictionary<int, TETile> tileinfo = new Dictionary<int, TETile>();
-        public static Dictionary<int, DEDoodad> doodadinfo = new Dictionary<int, DEDoodad>();
-        public static Dictionary<int, IEItem> iteminfo = new Dictionary<int, IEItem>();
-
-        public static TETile GetTile( int key )
-        {
-            if ( tileinfo.ContainsKey( key ) )
-            {
-                return tileinfo[key];
-            }
-            else
-            {
-                Debug.LogError( key + " isn't in the XMLLoader tileinfo dictionary" );
-                return tileinfo[0];
-            }
-        }
-        public static DEDoodad GetDoodad( int key )
-        {
-            if ( doodadinfo.ContainsKey( key ) )
-            {
-                return doodadinfo[key];
-            }
-            else
-            {
-                Debug.LogError( key + " isn't in the XMLLoader doodadinfo dictionary" );
-                return doodadinfo[0];
-            }
-        }
-        public static IEItem GetItem( int key )
-        {
-            if ( iteminfo.ContainsKey( key ) )
-            {
-                return iteminfo[key];
-            }
-            else
-            {
-                Debug.LogError( key + " isn't in the XMLLoader iteminfo dictionary" );
-                return iteminfo[0];
-            }
-        }
+        public static Dictionary<int, TETile> Tile = new Dictionary<int, TETile>();
+        public static Dictionary<int, DEDoodad> Doodad = new Dictionary<int, DEDoodad>();
+        public static Dictionary<int, IEItem> Item = new Dictionary<int, IEItem>();
 
         public static void Setup()
         {
@@ -64,7 +27,7 @@ namespace LowCloud.Reldawin
             stream.Close();
 
             foreach ( TETile tile in tileList.list ) {
-                tileinfo.Add( tile.id, tile );
+                Tile.Add( tile.id, tile );
             }
         }
 
@@ -76,7 +39,7 @@ namespace LowCloud.Reldawin
             stream.Close();
 
             foreach ( IEItem item in itemList.list ) {
-                iteminfo.Add( item.id, item );
+                Item.Add( item.id, item );
             }
         }
 
@@ -88,7 +51,7 @@ namespace LowCloud.Reldawin
             stream.Close();
 
             foreach ( DEDoodad doodad in doodadList.list ) {
-                doodadinfo.Add( doodad.id, doodad );
+                Doodad.Add( doodad.id, doodad );
             }
         }
     }
