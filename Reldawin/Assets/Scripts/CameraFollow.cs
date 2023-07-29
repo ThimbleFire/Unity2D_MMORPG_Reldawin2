51d419cff33259c3d93543730e9a722f29f1f4c3
Thimble <anthonyeboothroyd@gmail.com>
2023-07-29 17:05:09 +0100

    Fixed jumpy camera and sort mode

--- Reldawin/Assets/Scripts/CameraFollow.cs
+++ Reldawin/Assets/Scripts/CameraFollow.cs
@@ -16,7 +16,7 @@ namespace AlwaysEast
             if( followTarget != null ) {
                 targetPos = new Vector3( followTarget.position.x, followTarget.position.y, transform.position.z );
                 Vector3 velocity = (targetPos - transform.position) * moveSpeed;
-                transform.position = Vector3.SmoothDamp( transform.position, targetPos, ref velocity, 1.0f, Time.deltaTime );
+                transform.position = Vector3.SmoothDamp( transform.position, targetPos, ref velocity, 0.25f, Time.deltaTime );
             }
         }
     }
--- ReldawinServerMaster/ReldawinServerMaster/MapData.cs
+++ ReldawinServerMaster/ReldawinServerMaster/MapData.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.Diagnostics;
 using System.Drawing;
 using System.Reflection.Metadata.Ecma335;
 
@@ -7,8 +8,10 @@ namespace ReldawinServerMaster
     internal class MapData
     {
         private static Dictionary<Color, byte> colorDictionary = new Dictionary<Color, byte>();
+        private static Bitmap map;
 
         public static void Setup() {
+            map = new("map2.png");
             colorDictionary.Add( Color.FromArgb( 255, 93, 118, 203 ), 0 ); //water
             colorDictionary.Add( Color.FromArgb( 255, 113, 170, 52 ), 1 ); //grass
             colorDictionary.Add( Color.FromArgb( 255, 227, 207, 87 ), 2 ); //sand
@@ -18,7 +21,6 @@ namespace ReldawinServerMaster
         }
 
         public static string GetChunk( int chunkX, int chunkY ) {
-            Bitmap bmp = new("map2.png");
             string data = string.Empty;
 
             // Unity treats zero, zero as the bottom-left
@@ -26,13 +28,15 @@ namespace ReldawinServerMaster
             // Therefore we have to offset this
 
             int xStart = chunkX * 20;
-            int yStart = 999 -(chunkY * 20);
+            int yStart = (map.Height-1) -(chunkY * 20);
             int xLim = xStart + 20;
             int yLim = yStart - 20;
 
+            // There's no limit to stop us from trying to load chunks out of bounds.
+
             for( int y = yStart; y > yLim; y-- ) {
                 for( int x = xStart; x < xLim; x++ ) {
-                    data += colorDictionary[bmp.GetPixel( x, y )];
+                    data += colorDictionary[map.GetPixel( x, y )];
                 }
             }
             return data;
