1cd82ee7e53064ec72e095fd78c09b47c8fd7038
Thimble <anthonyeboothroyd@gmail.com>
2023-07-31 02:57:10 +0100

    Update World.cs

--- Reldawin/Assets/Scripts/World.cs
+++ Reldawin/Assets/Scripts/World.cs
@@ -1,4 +1,4 @@
-/*   Reldawin-0.3::World.cs
+/*   Reldawin/Assets/Scripts/World.cs
 /    Author: Tony Boothroyd
 /    Created: 21/07/2023
 /    Description: 
@@ -18,62 +18,6 @@ using Color = UnityEngine.Color;
 
 namespace AlwaysEast
 {
-    [Serializable]
-    public class Tile
-    {
-        public char associatedCharacter;
-        public List<TileBase> tileBases;
-
-        public const byte Width = 64;
-        public const byte Height = 31;
-    }
-    public interface IChunk { Vector3Int _index { get; } }
-    public class Chunk : IChunk
-    {
-        public static event ChunkDestroyImminent OnChunkDestroyed;
-        public delegate void ChunkDestroyImminent( Vector3Int chunkIndex, List<SceneObject> sceneObjects );
-
-        public const byte width = 20;
-        public const byte height = 20;
-        public Vector3Int Index { get; set; }
-        public Vector3Int _index { get { return Index; } }
-        public Node[,] Nodes { get; set; } = new Node[width, height];
-        public List<SceneObject> activeSceneObjects = new List<SceneObject>();
-        
-        public Chunk() {
-            for( int y = 0; y < height; y++ )
-            for( int x = 0; x <  width; x++ )
-                Nodes[x, y] = new Node( this, new Vector3Int( x, y) );
-        }
-        public void Erase(Tilemap tileMap) {
-            // Is erasing tiles neccesary? They'll just be overwritten when Reload is called.
-            //for( int y = 0; y < height; y++ )
-            //for( int x = 0; x < width; x++ )
-            //    tileMap.SetTile( Nodes[x, y].CellPositionInWorld, null );
-            activeSceneObjects.ForEach( x => x.gameObject.SetActive( false ) );
-            OnChunkDestroyed?.Invoke( Index, activeSceneObjects );
-            activeSceneObjects.Clear();
-        }
-        public void Reload( Tilemap tileMap, Vector3Int index, string data, List<SceneObjectData> sceneObjectData, List<SceneObject> inactiveSceneObjects ) {
-            Index = index;
-            int iteration = 0;
-            for( int y = 0; y < height; y++ )
-            for( int x = 0; x < width; x++ ) {
-                tileMap.SetTile(
-                    Nodes[x, y].CellPositionInWorld, 
-                    ResourceRepository.GetTilebaseOfType( data[iteration++] ) 
-                    );
-            }
-            foreach( SceneObjectData objectData in sceneObjectData ) {
-                Vector3 worldPosition = Nodes[objectData.x - ( index.x * Chunk.width ), objectData.y - ( index.y * Chunk.height )].WorldPosition;
-                inactiveSceneObjects[0].Setup( ResourceRepository.GetSprite( objectData.Type ) );
-                inactiveSceneObjects[0].transform.position = worldPosition;
-                activeSceneObjects.Add( inactiveSceneObjects[0] );
-                inactiveSceneObjects.RemoveAt( 0 );
-            }
-        }
-    }
-
     public class World : SceneBehaviour
     {
         public static event ClickAction OnClicked;
