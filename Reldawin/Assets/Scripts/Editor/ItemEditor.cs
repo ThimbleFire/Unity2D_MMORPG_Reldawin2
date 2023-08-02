a2b7d3f2223840ba5603d8d8846715ccb8cf724c
Thimble <anthonyeboothroyd@gmail.com>
2023-08-02 04:27:27 +0100

    Update ItemEditor.cs

--- Reldawin/Assets/Scripts/Editor/ItemEditor.cs
+++ Reldawin/Assets/Scripts/Editor/ItemEditor.cs
@@ -7,7 +7,50 @@ namespace AlwaysEast
 {
     public class ItemFileData
     {
-        public string binary;
+        [Flags]
+        public enum TAffixes : short
+        {
+            Plus_Accuracy =          0b00000000,
+            Dmg_Phys_Min =           0b00000001,
+            Dmg_Phys_Max =           0b00000010,
+            Dmg_Phys_Percent =       0b00000011,
+            Dmg_Ele_Fire =           0b00000100,
+            Dmg_Ele_Cold =           0b00000101,
+            Dmg_Ele_Lightning =      0b00000110,
+            Dmg_Ele_Poison =         0b00000111,
+            Def_Phys_Flat =          0b00001000,
+            Def_Phys_Percent =       0b00001001,
+            Def_Dmg_Reduction_Phys = 0b00001010,
+            Def_Dmg_Reduction_Magic =0b00001011,
+            Def_Dmg_Reduction_All =  0b00001100,
+            Def_Ele_Res_Fire =       0b00001101,
+            Def_Ele_Res_Cold =       0b00001110,
+            Def_Ele_Res_Lightning =  0b00001111,
+            Def_Ele_Res_Poison =     0b00010000,
+            Def_Ele_Res_All =        0b00010001,
+            On_Hit_Life =            0b00010010,
+            On_Kill_Life =           0b00010011,
+            Plus_Durability =        0b00010100,
+            On_Hit_Mana =            0b00010101,
+            Plus_Life =              0b00010110,
+            Plus_Mana =              0b00010111,
+            Plus_Regen_Life =        0b00011000,
+            Plus_Regen_Mana =        0b00011001,
+            Plus_Str =               0b00011010,
+            Plus_Dex =               0b00011011,
+            Plus_Con =               0b00011100,
+            Plus_Int =               0b00011101,
+            Plus_Speed_Phys =        0b00011110,
+            Plus_Speed_Magic =       0b00011111,
+            Plus_Speed_Movement =    0b00100000,
+            Plus_Block_Recovery =    0b00100001,
+            Plus_Stagger_Recovery =  0b00100010,
+            Plus_Magic_Find =        0b00100011,
+            Prefix                 = 0b10000000,
+            Suffix                 = 0b01000000
+        }
+        
+        public decimal binary;
 
         public int Id { get { return Convert.ToInt32( binary.Substring( 0, 8 ) ); } }
     }
