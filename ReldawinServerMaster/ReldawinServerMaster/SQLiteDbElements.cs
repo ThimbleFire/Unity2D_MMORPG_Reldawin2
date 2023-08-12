69307139484663d40f7ad521d66e6fb69a29cf32
Thimble <anthonyeboothroyd@gmail.com>
2023-08-12 05:17:28 +0100

    Update SQLiteDbElements.cs

--- ReldawinServerMaster/ReldawinServerMaster/SQLiteDbElements.cs
+++ ReldawinServerMaster/ReldawinServerMaster/SQLiteDbElements.cs
@@ -5,7 +5,6 @@ namespace ReldawinServerMaster
     public class EntityCoordinates
     {
         public int CoordinateX { get; set; }
-
         public int CoordinateY { get; set; }
 
         [PrimaryKey, AutoIncrement]
@@ -14,12 +13,23 @@ namespace ReldawinServerMaster
 
     public class Item
     {
-        public string Binary { get; set; }
 
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
 
+        public byte identifier { get; set; }
+        public byte value { get; set; }
+        public byte dura { get; set; }
+        public byte affix { get; set; }
+        public byte prefix1 { get; set; }
+        public byte prefix2 { get; set; }
+        public byte prefix3 { get; set; }
+        public byte suffix1 { get; set; }
+        public byte suffix2 { get; set; }
+        public byte suffix3 { get; set; }
+
         public int OwnerID { get; set; }
+        public byte inventoryPos { get; set; }
     }
 
     public class MapOreVeins
